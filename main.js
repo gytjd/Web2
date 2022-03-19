5a7c36977bcb32cd32b552b0e4d99a4ae16149d7
blue <101933437+gytjd@users.noreply.github.com>
2022-03-19 23:50:34 +0900

    인터렉티브 웹

---
+++ main.js
@@ -0,0 +1,33 @@
+(() => {
+
+  const stepElems = document.querySelectorAll('.step');
+  const graphicElems = document.querySelectorAll('.graphic-item');
+  let currentItem; // 현재 활성화된  .graphic-item
+
+  for (let i=0; i<stepElems.length;i++) {
+    //stepElems[i].setAttribute('data-index',i);
+    stepElems[i].dataset.index = i;
+    graphicElems[i].dataset.index = i;
+  }
+
+  window.addEventListener('scroll', () => {
+    let step;
+    let boundingRect;
+
+    for (let i=0;i<stepElems.length;i++) {
+      step=stepElems[i];
+      boundingRect=step.getBoundingClientRect();
+
+      if(boundingRect.top > window.innerHeight * 0.1
+         && boundingRect.top < window.innerHeight * 0.8) {
+
+          if(currentItem) {
+            currentItem.classList.remove('visible');
+          }
+         currentItem = graphicElems[step.dataset.index];
+         currentItem.classList.add('visible');
+      }
+    }
+  });
+
+})();
